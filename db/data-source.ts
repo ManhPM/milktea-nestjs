8edcb21989b7d26dd813f7b37e08e849aaaac5bd
ManhPM <phammanhbeo2001@gmail.com>
2023-10-22 22:16:44 +0700

    final

--- db/data-source.ts
+++ db/data-source.ts
@@ -9,8 +9,8 @@ export const dataSourceOptions: DataSourceOptions = {
   username: dev ? 'root' : 'uy3tjedf7fsdcssc',
   password: dev ? '15092001' : 't7khjsfkB5Hi2m2sgvuh',
   database: dev ? 'milktea-nestjs' : 'brtzudg8yfixwo1kmee0',
-  entities: ['../dist/**/*.entity.js'],
-  migrations: ['../dist/db/migrations/*.js'],
+  entities: ['../src/**/*.entity.js'],
+  migrations: ['../src/db/migrations/*.js'],
   synchronize: false,
   timezone: 'Asia/Ho_Chi_Minh',
 };
