5a06e16c1dbaab8de97275243debd4190c5e7199
ManhPM <phammanhbeo2001@gmail.com>
2023-10-24 12:55:43 +0700

    final

--- src/common/lib.ts
+++ src/common/lib.ts
@@ -5,14 +5,76 @@ import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
 import * as path from 'path';
 
+//DEV
+
+// @Injectable({ scope: Scope.REQUEST })
+// export class MessageService {
+//   constructor(@Inject(REQUEST) private readonly request: Request) {}
+
+//   async getMessage(messageCode: any) {
+//     try {
+//       const workbook = new Excel.Workbook();
+//       await workbook.xlsx.readFile('src/common/message.xlsx');
+//       const worksheet = workbook.getWorksheet('Sheet1');
+//       let message;
+//       let language = 'VN';
+//       if (this.request.query.language) {
+//         language = this.request.query.language as string;
+//       }
+//       for (let i = 2; i <= worksheet.rowCount; i++) {
+//         const firstRow = worksheet.getRow(1);
+//         const row = worksheet.getRow(i);
+//         for (let i = 2; i <= worksheet.columnCount; i++) {
+//           if (
+//             firstRow.getCell(i).value == language &&
+//             row.getCell(1).value == messageCode
+//           ) {
+//             message = row.getCell(i).value;
+//             break;
+//           }
+//         }
+//       }
+//       if (!message) {
+//         throw new HttpException(
+//           {
+//             messageCode: 'MESSAGE_NOTFOUND',
+//           },
+//           HttpStatus.BAD_REQUEST,
+//         );
+//       } else {
+//         return message;
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       if (error.response.messageCode) {
+//         const message = await this.getMessage(error.response.messageCode);
+//         throw new HttpException(
+//           {
+//             message: message,
+//           },
+//           HttpStatus.BAD_REQUEST,
+//         );
+//       } else {
+//         const message = await this.getMessage('INTERNAL_SERVER_ERROR');
+//         throw new HttpException(
+//           {
+//             message: message,
+//           },
+//           HttpStatus.INTERNAL_SERVER_ERROR,
+//         );
+//       }
+//     }
+//   }
+// }
+
+//PRODUCTION
 @Injectable({ scope: Scope.REQUEST })
 export class MessageService {
   constructor(@Inject(REQUEST) private readonly request: Request) {}
-
   async getMessage(messageCode: any) {
     try {
       const workbook = new Excel.Workbook();
-      await workbook.xlsx.readFile('src/common/message.xlsx');
+      await workbook.xlsx.readFile('vercel/path0/src/common/message.xlsx');
       const worksheet = workbook.getWorksheet('Sheet1');
       let message;
       let language = 'VN';
