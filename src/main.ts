4e95685a943244ac989172531c2c28a125457dca
ManhPM <phammanhbeo2001@gmail.com>
2023-10-25 23:13:17 +0700

    final

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use((cookieParserConfig as any)());
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: process.env.ENV === 'dev' ? true : 'http://localhost:3003',
+    origin: true,
     credentials: true,
   });
   await app.listen(4000);
