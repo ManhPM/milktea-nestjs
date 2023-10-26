556c48258125681111bd56cd5066ddfcf4f26f17
ManhPM <phammanhbeo2001@gmail.com>
2023-10-26 10:22:04 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use((cookieParserConfig as any)());
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: process.env.ENV === 'dev' ? true : 'http://localhost:3003',
+    origin: process.env.ENV === 'dev' ? true : 'https://tea-z.vercel.app/',
     credentials: true,
   });
   await app.listen(4000);
