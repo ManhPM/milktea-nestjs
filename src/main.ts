2071b1be98e6cba04102eb68b58e301faa4962f3
ManhPM <phammanhbeo2001@gmail.com>
2023-10-26 11:32:15 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParserConfig = require('cookie-parser');
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,11 +9,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use((cookieParserConfig as any)());
   app.setGlobalPrefix('api/v1');
-  const corsOptions = {
-    origin: process.env.ENV === 'dev' ? true : 'https://tea-z.vercel.app/',
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
+  app.enableCors({
+    origin: function (origin, callback) {
+      callback(null, true);
+    },
+  });
+  await app.listen(3000);
   await app.listen(4000);
 }
 bootstrap();
