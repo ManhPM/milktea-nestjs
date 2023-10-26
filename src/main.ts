30925f0b22840a94491be625bc08ab70a85e00d8
ManhPM <phammanhbeo2001@gmail.com>
2023-10-26 12:48:35 +0700

    final

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   app.enableCors({
     origin: process.env.ENV === 'dev' ? true : 'https://tea-z.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: '*',
     credentials: true,
-    preflightContinue: true,
   });
   await app.listen(4000);
 }
