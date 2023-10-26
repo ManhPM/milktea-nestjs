4fdb8213a227e516ec09a603971345ce03d6ce19
ManhPM <phammanhbeo2001@gmail.com>
2023-10-26 11:29:39 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParserConfig = require('cookie-parser');
 import { ValidationPipe } from '@nestjs/common';
-import cors from 'cors';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
