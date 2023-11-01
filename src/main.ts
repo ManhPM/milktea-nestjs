4f8abf90e8f59d9c89e7536b4fbde1b730f23225
ManhPM <phammanhbeo2001@gmail.com>
2023-11-01 20:17:11 +0700

    final

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,11 @@ async function bootstrap() {
     credentials: true,
   });
   app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-  next();
-});
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   await app.listen(4000);
 }
 bootstrap();
