217e8f7d26219a816c913bce1da231749f4f254a
ManhPM <phammanhbeo2001@gmail.com>
2023-11-01 23:49:18 +0700

    final

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParserConfig = require('cookie-parser');
 import { ValidationPipe } from '@nestjs/common';
+import { NextFunction, Response, Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,13 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.ENV === 'dev' ? true : 'https://tea-z-navy.vercel.app/',
     credentials: true,
+  });
+  app.use(function (request: Request, response: Response, next: NextFunction) {
+    response.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://tea-z-navy.vercel.app/',
+    );
+    next();
   });
   await app.listen(4000);
 }
