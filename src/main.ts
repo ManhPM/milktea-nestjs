f7d50f9bea8c39fdf481574f9969303dbd990608
ManhPM <phammanhbeo2001@gmail.com>
2023-11-01 20:21:25 +0700

    final

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,16 @@ async function bootstrap() {
   app.use((cookieParserConfig as any)());
   app.setGlobalPrefix('api/v1');
 
-  app.enableCors({
-    origin: ['https://tea-z-navy.vercel.app'],
-    methods: ['GET', 'POST', 'HEAD', 'PATCH', 'PUT', 'DELETE'],
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    credentials: true,
-  });
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
   });
   await app.listen(4000);
 }
