b8d37c2c1efa9f859b03be46e37d649b9db3566c
ManhPM <phammanhbeo2001@gmail.com>
2023-11-03 20:02:49 +0700

    deploy

--- dist/src/main.js
+++ dist/src/main.js
@@ -5,7 +5,7 @@ const app_module_1 = require("./app.module");
 const cookieParserConfig = require("cookie-parser");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: false });
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.use(cookieParserConfig());
     app.setGlobalPrefix('api/v1');
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import cookieParserConfig = require('cookie-parser');
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
 
   app.useGlobalPipes(new ValidationPipe());
   app.use((cookieParserConfig as any)());
